b808f3286f55377dce6b5a4df87aa876eafa41b6
Lucas Olmedo Silva <lcsolmedosilva@gmail.com>
2025-02-26 12:12:30 -0300

    creates type Exercise and refactors exercise.tsx

--- app/(tabs)/exercises.tsx
+++ app/(tabs)/exercises.tsx
@@ -1,22 +1,70 @@
-import { View, ScrollView, StyleSheet } from 'react-native';
-import { useState } from 'react';
-import { Card, Chip, Divider, List, Searchbar, Text, useTheme } from 'react-native-paper';
+import { View, ScrollView, StyleSheet, Image } from 'react-native';
+import { useState, useMemo } from 'react';
+import { Card, Chip, List, Searchbar, Text, useTheme } from 'react-native-paper';
 import { baseStyles } from '@/theme/baseStyle';
-import { Image } from 'react-native';
+import { Exercise } from '@/types/exercise';
 
-const exercises = [
+const FilterChips = ({
+  items,
+  selectedItems,
+  onSelect,
+  type
+}: {
+  items: string[],
+  selectedItems: string[],
+  onSelect: (item: string, type: 'category' | 'equipment') => void,
+  type: 'category' | 'equipment'
+}) => (
+  <View style={styles.categoryFilter}>
+    <ScrollView horizontal showsHorizontalScrollIndicator={false}
+      style={baseStyles.chipGroup}
+    >
+      {items.map(item => (
+        <Chip
+          key={item}
+          selected={selectedItems.includes(item)}
+          onPress={() => onSelect(item, type)}
+          style={baseStyles.chip}
+          showSelectedOverlay
+        >
+          {item}
+        </Chip>
+      ))}
+    </ScrollView>
+  </View>
+);
+
+const ExerciseItem = ({ exercise }: { exercise: Exercise }) => (
+  <List.Item
+    key={exercise.id}
+    title={exercise.name}
+    description={exercise.bodyPart}
+    onPress={() => { }}
+    left={() => (
+      <Image
+        source={{ uri: exercise.gifUrl }}
+        style={styles.exerciseImage}
+        resizeMode="cover"
+        fadeDuration={0}
+      />
+    )}
+    titleStyle={styles.exerciseName}
+    descriptionStyle={styles.exerciseCategory}
+  />
+);
+const exercises: Exercise[] = [
   {
-    "bodyPart": "waist",
-    "equipment": "body weight",
-    "gifUrl": "https://v2.exercisedb.io/image/OQtEmeOqzII9KN",
-    "id": "0001",
-    "name": "3/4 sit-up",
-    "target": "abs",
-    "secondaryMuscles": [
+    bodyPart: "waist",
+    equipment: "body weight",
+    gifUrl: "https://v2.exercisedb.io/image/OQtEmeOqzII9KN",
+    id: "0001",
+    name: "3/4 sit-up",
+    target: "abs",
+    secondaryMuscles: [
       "hip flexors",
       "lower back"
     ],
-    "instructions": [
+    instructions: [
       "Lie flat on your back with your knees bent and feet flat on the ground.",
       "Place your hands behind your head with your elbows pointing outwards.",
       "Engaging your abs, slowly lift your upper body off the ground, curling forward until your torso is at a 45-degree angle.",
@@ -25,16 +73,16 @@ const exercises = [
     ]
   },
   {
-    "bodyPart": "waist",
-    "equipment": "body weight",
-    "gifUrl": "https://v2.exercisedb.io/image/n5PoRUvOh7iM0R",
-    "id": "0002",
-    "name": "45° side bend",
-    "target": "abs",
-    "secondaryMuscles": [
+    bodyPart: "waist",
+    equipment: "body weight",
+    gifUrl: "https://v2.exercisedb.io/image/n5PoRUvOh7iM0R",
+    id: "0002",
+    name: "45° side bend",
+    target: "abs",
+    secondaryMuscles: [
       "obliques"
     ],
-    "instructions": [
+    instructions: [
       "Stand with your feet shoulder-width apart and your arms extended straight down by your sides.",
       "Keeping your back straight and your core engaged, slowly bend your torso to one side, lowering your hand towards your knee.",
       "Pause for a moment at the bottom, then slowly return to the starting position.",
@@ -43,16 +91,16 @@ const exercises = [
     ]
   },
   {
-    "bodyPart": "waist",
-    "equipment": "body weight",
-    "gifUrl": "https://v2.exercisedb.io/image/XhnGyFI1XoGvdk",
-    "id": "0003",
-    "name": "air bike",
-    "target": "abs",
-    "secondaryMuscles": [
+    bodyPart: "waist",
+    equipment: "body weight",
+    gifUrl: "https://v2.exercisedb.io/image/XhnGyFI1XoGvdk",
+    id: "0003",
+    name: "air bike",
+    target: "abs",
+    secondaryMuscles: [
       "hip flexors"
     ],
-    "instructions": [
+    instructions: [
       "Lie flat on your back with your hands placed behind your head.",
       "Lift your legs off the ground and bend your knees at a 90-degree angle.",
       "Bring your right elbow towards your left knee while simultaneously straightening your right leg.",
@@ -61,16 +109,16 @@ const exercises = [
     ]
   },
   {
-    "bodyPart": "waist",
-    "equipment": "body weight",
-    "gifUrl": "https://v2.exercisedb.io/image/FEQR2igjfVBeuK",
-    "id": "0006",
-    "name": "alternate heel touchers",
-    "target": "abs",
-    "secondaryMuscles": [
+    bodyPart: "waist",
+    equipment: "body weight",
+    gifUrl: "https://v2.exercisedb.io/image/FEQR2igjfVBeuK",
+    id: "0006",
+    name: "alternate heel touchers",
+    target: "abs",
+    secondaryMuscles: [
       "obliques"
     ],
-    "instructions": [
+    instructions: [
       "Lie flat on your back with your knees bent and feet flat on the ground.",
       "Extend your arms straight out to the sides, parallel to the ground.",
       "Engaging your abs, lift your shoulders off the ground and reach your right hand towards your right heel.",
@@ -79,17 +127,17 @@ const exercises = [
     ]
   },
   {
-    "bodyPart": "back",
-    "equipment": "cable",
-    "gifUrl": "https://v2.exercisedb.io/image/6icN20v65O9pgQ",
-    "id": "0007",
-    "name": "alternate lateral pulldown",
-    "target": "lats",
-    "secondaryMuscles": [
+    bodyPart: "back",
+    equipment: "cable",
+    gifUrl: "https://v2.exercisedb.io/image/6icN20v65O9pgQ",
+    id: "0007",
+    name: "alternate lateral pulldown",
+    target: "lats",
+    secondaryMuscles: [
       "biceps",
       "rhomboids"
     ],
-    "instructions": [
+    instructions: [
       "Sit on the cable machine with your back straight and feet flat on the ground.",
       "Grasp the handles with an overhand grip, slightly wider than shoulder-width apart.",
       "Lean back slightly and pull the handles towards your chest, squeezing your shoulder blades together.",
@@ -99,54 +147,63 @@ const exercises = [
   }
 ];
 
-const categories = ['All', ...new Set(exercises.map(exercise => exercise.bodyPart))];
-
-const equipments = ['All', ...new Set(exercises.map(exercise => exercise.equipment))];
-
 const ExercisesPage = () => {
   const theme = useTheme();
   const [searchQuery, setSearchQuery] = useState('');
   const [selectedCategories, setSelectedCategories] = useState<string[]>(['All']);
   const [selectedEquipments, setSelectedEquipments] = useState<string[]>(['All']);
-  const filteredExercises = selectedCategories.includes('All')
-    && selectedEquipments.includes('All') &&
-    !searchQuery
-    ? exercises
-    : exercises.filter(exercise =>
-      (selectedCategories.includes('All') || selectedCategories.includes(exercise.bodyPart)) &&
-      (selectedEquipments.includes('All') || selectedEquipments.includes(exercise.equipment)) &&
-      (exercise.name.toLowerCase().includes(searchQuery.toLowerCase()))
-    );
 
-  const applyFilter = (filter: string, type: 'category' | 'equipment') => {
-    if (type === 'category') {
-      if (filter === 'All') {
-        setSelectedCategories(['All']);
-        return;
-      }
+  const categories = useMemo((): string[] =>
+    ['All', ...new Set<string>(exercises.map(exercise => exercise.bodyPart))],
+    []
+  );
 
-      const updatedCategories = selectedCategories.includes(filter)
-        ? selectedCategories.filter(c => c !== filter)
-        : [...selectedCategories.filter(c => c !== 'All'), filter];
+  const equipments = useMemo((): string[] =>
+    ['All', ...new Set<string>(exercises.map(exercise => exercise.equipment))],
+    []
+  );
 
-      setSelectedCategories(updatedCategories.length ? updatedCategories : ['All']);
-    } else {
-      if (filter === 'All') {
-        setSelectedEquipments(['All']);
-        return;
-      }
+  const handleFilter = (filter: string, type: 'category' | 'equipment') => {
+    const setFilter = type === 'category'
+      ? setSelectedCategories : setSelectedEquipments;
 
-      const updatedEquipments = selectedEquipments.includes(filter)
-        ? selectedEquipments.filter(c => c !== filter)
-        : [...selectedEquipments.filter(c => c !== 'All'), filter];
+    const currentSelection = type === 'category'
+      ? selectedCategories : selectedEquipments;
 
-      setSelectedEquipments(updatedEquipments.length ? updatedEquipments : ['All']);
+    if (filter === 'All') {
+      setFilter(['All']);
+      return;
     }
+
+    const updatedSelection = currentSelection.includes(filter)
+      ? currentSelection.filter(item => item !== filter)
+      : [...currentSelection.filter(item => item !== 'All'), filter];
+
+    setFilter(updatedSelection.length ? updatedSelection : ['All']);
   };
 
+  const filteredExercises = useMemo(() => {
+    const isDefaultFilter = selectedCategories.includes('All') &&
+      selectedEquipments.includes('All') &&
+      !searchQuery;
+
+    if (isDefaultFilter) return exercises;
+
+    return exercises.filter(exercise => (
+      (selectedCategories.includes('All')
+        || selectedCategories.includes(exercise.bodyPart)) &&
+      (selectedEquipments.includes('All')
+        || selectedEquipments.includes(exercise.equipment)) &&
+      exercise.name.toLowerCase().includes(searchQuery.toLowerCase())
+    ));
+  }, [selectedCategories, selectedEquipments, searchQuery]);
+
   return (
-    <ScrollView style={[baseStyles.container, { backgroundColor: theme.colors.background }]}>
+    <ScrollView style={[baseStyles.container,
+    { backgroundColor: theme.colors.background }]}
+    >
       <Text style={baseStyles.pageHeader}>Exercícios</Text>
+
       <Searchbar
         placeholder="Search exercises"
         onChangeText={setSearchQuery}
@@ -156,65 +213,26 @@ const ExercisesPage = () => {
       />
 
       <View style={styles.container}>
-        <View style={styles.categoryFilter}>
-          <ScrollView
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            style={baseStyles.chipGroup}
-          >
-            {equipments.map(equipment => (
-              <Chip
-                key={equipment}
-                selected={selectedEquipments.includes(equipment)}
-                onPress={() => applyFilter(equipment, 'equipment')}
-                style={baseStyles.chip}
-                showSelectedOverlay
-              >
-                {equipment}
-              </Chip>
-            ))}
-          </ScrollView>
-        </View>
+        <FilterChips
+          items={equipments}
+          selectedItems={selectedEquipments}
+          onSelect={handleFilter}
+          type="equipment"
+        />
 
-        <View style={styles.categoryFilter}>
-          <ScrollView
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            style={baseStyles.chipGroup}
-          >
-            {categories.map(category => (
-              <Chip
-                key={category}
-                selected={selectedCategories.includes(category)}
-                onPress={() => applyFilter(category, 'category')}
-                style={baseStyles.chip}
-                showSelectedOverlay
-              >
-                {category}
-              </Chip>
-            ))}
-          </ScrollView>
-        </View>
+        <FilterChips
+          items={categories}
+          selectedItems={selectedCategories}
+          onSelect={handleFilter}
+          type="category"
+        />
 
         <ScrollView style={styles.exerciseList}>
           {filteredExercises.length > 0 && (
             <Card style={styles.exerciseList}>
               <Card.Content>
                 {filteredExercises.map(exercise => (
-                  <List.Item
-                    key={exercise.id}
-                    title={exercise.name}
-                    description={exercise.bodyPart}
-                    onPress={() => { }}
-                    left={() => (
-                      <Image
-                        source={{ uri: exercise.gifUrl }}
-                        style={styles.exerciseImage}
-                      />
-                    )}
-                    titleStyle={styles.exerciseName}
-                    descriptionStyle={styles.exerciseCategory}
-                  />
+                  <ExerciseItem key={exercise.id} exercise={exercise} />
                 ))}
               </Card.Content>
             </Card>
